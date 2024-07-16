4ba090f3db449382684f0c7af4c5b437aa7d8d02
wake_lift <ad-noctum@yandex.ru>
2024-07-16 14:26:11 +0300

    Add Elasticsearch to ARM64 docker-compose. Optimize get random questions queries

--- app/crud/questions_pages.py
+++ app/crud/questions_pages.py
@@ -2,7 +2,7 @@ from datetime import datetime, timezone
 import random
 
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy import Select, Sequence, false, func, select, true
+from sqlalchemy import Select, Sequence, false, func, or_, select, true
 
 from app.core.constants import REFRESH_INTERVAL, SET_FOR_RANDOMIZING
 from app.models.questions import Question
@@ -15,7 +15,7 @@ LAST_REFRESH: datetime = datetime.now(timezone.utc)
 
 async def get_max_question_sets(
         session:  AsyncSession
-) -> dict[str, int | None]:
+) -> dict[str, int]:
     """Пересчитывает количество вопросов в БД по трем категориям через
     заданный в параметре REFRESH_INTERVAL интервал времени."""
     global MAX_SET_WWW, MAX_SET_BRAIN, MAX_SET_JEOPARDY, LAST_REFRESH
@@ -23,19 +23,26 @@ async def get_max_question_sets(
         not all([MAX_SET_JEOPARDY, MAX_SET_BRAIN, MAX_SET_WWW,])
         or (datetime.now(timezone.utc) - LAST_REFRESH > REFRESH_INTERVAL)
     ):
-        def get_local_query(question_type: str) -> Select:
-            return (
-                select(func.count())
-                .select_from(Question)
-                .filter(
-                    Question.is_condemned == false(),
-                    Question.is_published == true(),
-                    Question.question_type == question_type
+        query = (
+            select(Question.question_type, func.count(Question.question))
+            .filter(
+                Question.is_condemned == false(),
+                Question.is_published == true(),
+            )
+            .group_by(Question.question_type)
+            .having(
+                or_(
+                    Question.question_type == 'Ч',
+                    Question.question_type == 'Б',
+                    Question.question_type == 'Я'
                 )
             )
-        MAX_SET_WWW = await session.scalar(get_local_query('Ч'))
-        MAX_SET_BRAIN = await session.scalar(get_local_query('Б'))
-        MAX_SET_JEOPARDY = await session.scalar(get_local_query('Я'))
+        )
+        question_type_counts = await session.execute(query)
+        counts = {_[0].value: _[1] for _ in question_type_counts.all()}
+        MAX_SET_WWW = counts['Что-где-когда']
+        MAX_SET_BRAIN = counts['Брейн-ринг']
+        MAX_SET_JEOPARDY = counts['Своя игра']
         LAST_REFRESH = datetime.now(timezone.utc)
     return {
         'Ч': MAX_SET_WWW,
@@ -68,9 +75,6 @@ async def get_initial_random_question_set(
     """Формирует первоначальный список вопросов с учетом типа вопроса и
     заданного константой "SET_FOR_RANDOMIZING" размера выборки.
     Функция необходима для ускорения запросов к БД."""
-    query = get_base_query(question_type)
-    questions = await session.execute(query)
-    questions = questions.all()
     max_question_sets = await get_max_question_sets(session)
     questions_quantity = max_question_sets[question_type]
     randomizing_limit = questions_quantity - SET_FOR_RANDOMIZING - 1
@@ -80,7 +84,12 @@ async def get_initial_random_question_set(
     )
     if questions_quantity < SET_FOR_RANDOMIZING:
         start_point = 0
-    return questions[start_point: start_point + SET_FOR_RANDOMIZING]
+    questions = await session.execute(
+        get_base_query(question_type)
+        .limit(SET_FOR_RANDOMIZING)
+        .offset(start_point)
+    )
+    return questions.all()
 
 
 async def get_random_package(
